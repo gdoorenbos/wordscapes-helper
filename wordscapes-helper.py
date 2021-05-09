31631c3daa5c59fc3cbe36877ca64036407a1dec
Garrett Doorenbos <garrett.doorenbos@gmail.com>
2021-05-09 00:40:15 -0500

    get input from command line

--- wordscapes-helper.py
+++ wordscapes-helper.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 
 with open("words_alpha.txt") as word_file:
@@ -5,36 +6,42 @@ with open("words_alpha.txt") as word_file:
 
 def is_english_word(word):
     return word.lower() in english_words
-    
-def main():
-    while True:
-        word_pool = input('word pool: ')
-        word_len = int(input('length of word: '))
-        word_hint = input('word hints: ').lower()
-        matches = []
-        for word in english_words:
-            if len(word) == word_len:
-                word_fits = True
-                for i in range(word_len):
-                    if word[i] not in word_pool:
-                        word_fits = False
-                        break
-                    if word_hint[i] != '_' and word_hint[i] != word[i]:
+
+def main(word_pool, word_len, word_hint=None):
+    if word_hint is None:
+        word_hint = '_' * word_len
+    word_hint = word_hint.lower()
+
+    matches = []
+    for word in english_words:
+        if len(word) == word_len:
+            word_fits = True
+            for i in range(word_len):
+                if word[i] not in word_pool:
+                    word_fits = False
+                    break
+                if word_hint[i] != '_' and word_hint[i] != word[i]:
+                    word_fits = False
+                    break
+
+            if word_fits:
+                for letter in word:
+                    letter_count = len([x for x in word if x == letter])
+                    pool_count = len([x for x in word_pool if x == letter])
+                    if letter_count > pool_count:
                         word_fits = False
                         break
 
                 if word_fits:
-                    for letter in word:
-                        letter_count = len([x for x in word if x == letter])
-                        pool_count = len([x for x in word_pool if x == letter])
-                        if letter_count > pool_count:
-                            word_fits = False
-                            break
+                    matches.append(word)
 
-                    if word_fits:
-                        matches.append(word)
-
-        print('matches: {}'.format(matches))
+    print('matches: {}'.format(matches))
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--letter-pool', required=True, type=str)
+    parser.add_argument('-l', '--word-length', required=True, type=int)
+    parser.add_argument('-k', '--hints', type=str)
+    args = parser.parse_args()
+
+    main(args.letter_pool, args.word_length, args.hints)
